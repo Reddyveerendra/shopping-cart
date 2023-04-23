a91955564656498a36171edc9387f67484fba929
Reddyveerendra <reddyveerendra535@gmail.com>
2023-04-23 08:25:24 +0530

    images

---
+++ src/components/data.js
@@ -0,0 +1,58 @@
+export const data = {
+    0: {
+        name: "Harry Potter and the PhilosopherU+2019s Stone",
+        price: "₹799.00",
+        image: "",
+        description: "Harry Potter thinks he is an ordinary boy - until he is rescued by an owl, taken to Hogwarts School of Witchcraft and Wizardry, learns to play Quidditch and does battle in a deadly duel. The Reason ... HARRY POTTER IS A WIZARD!",
+        orders: 0
+    },
+    1: {
+        name: "Harry Potter and the Chamber of Secrets",
+        price: "849.00",
+        image: "",
+        description: "Ever since Harry Potter had come home for the summer, the Dursleys had been so mean and hideous that all Harry wanted was to get back to the Hogwarts School for Witchcraft and Wizardry. But just as heU+2019s packing his bags, Harry receives a warning from a strange impish creature who says that if Harry returns to Hogwarts, disaster will strike.And strike it does. For in Harry’s second year at Hogwarts, fresh torments and horrors arise, including an outrageously stuck-up new professor and a spirit who haunts the girlsU+2019 bathroom. But then the real trouble begins U+2013 someone is turning Hogwarts students to stone. Could it be Draco Maldon, a more poisonous rival than ever? Could it possibly be Hagrid, whose mysterious past is finally told? Or could it be the one everyone at Hogwarts most suspects… Harry Potter himself!",
+        orders: 0
+    },
+    2: {
+        name: "Harry Potter and the Prisoner of Azkaban",
+        price: "₹1,039.00",
+        image: "",
+        description: "Harry Potter, along with his best friends, Ron and Hermione, is about to start his third year at Hogwarts School of Witchcraft and Wizardry. Harry can't wait to get back to school after the summer holidays. (Who wouldn't if they lived with the horrible Dursleys?) But when Harry gets to Hogwarts, the atmosphere is tense. There's an escaped mass murderer on the loose, and the sinister prison guards of Azkaban have been called in to guard the school...",
+        orders: 0
+    },
+    3: {
+        name: "Harry Potter and the Goblet of Fire",
+        price: "₹849",
+        image: "",
+        description: "It is the summer holidays and soon Harry Potter will be starting his fourth year at Hogwarts School of Witchcraft and Wizardry. Harry is counting the days: there are new spells to be learnt, more Quidditch to be played, and Hogwarts castle to continue exploring. But Harry needs to be careful - there are unexpected dangers lurking...",
+        orders: 0
+    },
+    4: {
+        name: "Harry Potter and the Order of the Phoenix",
+        price: "800",
+        image: "",
+        description: "Harry Potter is about to start his fifth year at Hogwarts School of Witchcraft and Wizardry. Unlike most schoolboys, Harry never enjoys his summer holidays, but this summer is even worse than usual. The Dursleys, of course, are making his life a misery, but even his best friends, Ron and Hermione, seem to be neglecting him.Harry has had enough. He is beginning to think he must do something, anything, to change his situation, when the summer holidays come to an end in a very dramatic fashion. What Harry is about to discover in his new year at Hogwarts will turn his world upside down...",
+        orders: 0
+    },
+    5: {
+        name: "Harry Potter and the Half-Blood Prince",
+        price: "₹900",
+        image: "",
+        description: "It is the middle of the summer, but there is an unseasonal mist pressing against the windowpanes. Harry Potter is waiting nervously in his bedroom at the Dursleys' house in Privet Drive for a visit from Professor Dumbledore himself. One of the last times he saw the Headmaster was in a fierce one-to-one duel with Lord Voldemort, and Harry can't quite believe that Professor Dumbledore will actually appear at the Dursleys' of all places. Why is the Professor coming to visit him now? What is it that cannot wait until Harry returns to Hogwarts in a few weeks' time? Harry's sixth year at Hogwarts has already got off to an unusual start, as the worlds of Muggle and magic start to intertwine...",
+        orders: 0
+    },
+    6: {
+        name: "Harry Potter and the Deathly Hallows",
+        price: "750",
+        image: "",
+        description: "Harry has been burdened with a dark, dangerous and seemingly impossible task: that of locating and destroying Voldemort's remaining Horcruxes. Never has Harry felt so alone, or faced a future so full of shadows. But Harry must somehow find within himself the strength to complete the task he has been given. He must leave the warmth, safety and companionship of The Burrow and follow without fear or hesitation the inexorable path laid out for him... In this final, seventh installment of the Harry Potter series, J.K. Rowling unveils in spectacular fashion the answers to the many questions that have been so eagerly awaited.",
+        orders: 0
+    },
+    7: {
+        name: "Harry Potter and the Cursed Child: Parts One and Two",
+        price: "₹720",
+        image: "",
+        description: "The eighth story, nineteen years later... It was always difficult being Harry Potter, and it isn't much easier now that he is an overworked employee of the Ministry of Magic, a husband, and a father of three school-age children.",
+        orders: 0
+    }
+}
